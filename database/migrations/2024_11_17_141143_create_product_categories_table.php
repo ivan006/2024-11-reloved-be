b7471d8ca840ad0b32a39aad6fb0cf0bed4990b3
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-17 16:14:13 +0200

    1

--- database/migrations/2024_11_17_141143_create_product_categories_table.php
+++ database/migrations/2024_11_17_141143_create_product_categories_table.php
@@ -4,23 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+return new class extends Migration {
+    public function up()
     {
         Schema::create('product_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('product_categories');
     }
